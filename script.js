223fda55cfb04c23b470f37a8fb9967e527980b2
Dev-pavanlokesh <pavanlokesh.developer@gmail.com>
2023-12-13 16:19:36 +0530

    version 1

---
+++ script.js
@@ -0,0 +1,85 @@
+let boxes = document.querySelectorAll(".box");
+
+let reset = document.querySelector(".reset");
+
+let turn = true;
+
+const win = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+boxes.forEach((box) =>{
+    box.addEventListener("click",()=>{
+        console.log("clicked");
+        if(turn){
+            box.innerText = "O";
+            turn = false
+        }
+        else{
+            box.innerText = "X";
+            turn = true
+        }
+        box.disabled = true
+
+        checkwinner();
+    });
+});
+const enable = ()=>{
+    turn = true
+    for(box of boxes){
+        box.disabled= false;
+    }
+}
+const winner = (strin)=>{
+    Swal.fire({
+        title: 'Tic Tac Toe!',
+        text: `The winner is ${strin}`,
+        icon: 'success',
+        confirmButtonText: 'Play another game',
+      });
+      
+    resetfunc();
+    enable();
+}
+const resetfunc = ()=>{
+    for(let box of boxes){
+        box.disabled = true;
+        box.innerText = "";
+    }
+};
+
+
+
+
+reset.addEventListener("click",()=>{
+    for(let box of boxes){
+        box.disabled = true;
+        box.innerText = "";
+    }
+    enable();
+})
+var delayy = 100;
+
+const checkwinner = () => {
+    for(let pattern of  win){
+      let  pos1 = boxes[pattern[0]].innerText;
+      let  pos2 = boxes[pattern[1]].innerText;
+      let  pos3 = boxes[pattern[2]].innerText;
+
+      if(pos1 != "" && pos2 != "" && pos3 != ""){
+        if(pos1 === pos2 && pos2 === pos3){
+
+            setTimeout(()=>{
+                winner(pos1);
+            },delayy);
+        }
+      }
+    }
+};
